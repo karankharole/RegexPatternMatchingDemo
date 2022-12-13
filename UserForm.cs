d7cac49c882d07cf56a2c55a3ebef593f0d5597a
karankharole <karankharole2000@gmail.com>
2022-12-13 18:10:18 +0530

    validate the email

--- UserForm.cs
+++ UserForm.cs
@@ -12,32 +12,51 @@ namespace RegexPatternMatchingDemo
         public static void userForm()
         {
             string first = @"^[A-Z]{1}[a-z A-Z]{2,10}";
+            string emailPattern = @"^[a-z A-Z]+([.][a-z A-Z]+)*@[a-z A-Z]+.[a-z A-Z]{2,10}([.][a-z A-Z]{2,3})?$";
+
             Console.Write("Enter first name : ");
             string fName = Console.ReadLine();
             Console.Write("Enter last name : ");
             string lName = Console.ReadLine();
+            Console.Write("Enter your email address : ");
+            string email = Console.ReadLine();
 
             Regex name = new Regex(first);
 
+            Regex emailValidate = new Regex(emailPattern);
+
             bool frst = name.IsMatch(fName);
             bool lst = name.IsMatch(lName);
+            bool emailResult = emailValidate.IsMatch(email);
+
+
+
 
             if (frst)
             {
-                Console.WriteLine($" Valide First Name {fName}");
+                Console.WriteLine($" Valid First Name {fName}");
             }
             else
             {
-                Console.WriteLine("Invalide first name");
+                Console.WriteLine("Invalid first name");
             }
             if (lst)
             {
-                Console.WriteLine($" Valide Last Name {lName}");
+                Console.WriteLine($" Valid Last Name {lName}");
             }
             else
             {
                 Console.WriteLine("Invalide last name");
             }
+            if (emailResult)
+            {
+                Console.WriteLine($" Valid email  {email}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email");
+            }
+
 
         }
     }
